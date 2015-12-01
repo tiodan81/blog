645c5369acb65017afc9f44eae1573a61987c862
Dan Schwartz <dpschwartz@gmail.com>
2015-12-01 13:11:12 -0800

    Hide and show non-first paragraphs of articles.

--- js/blog.js
+++ js/blog.js
@@ -19,13 +19,26 @@ var blog = {
     blog.articles.forEach(function(a) {
       a.toHTML();
     });
+    $('#template').remove();
+  },
+
+  hideArticles: function() {
+    var $ps = $('.body p:not(:first-child)');
+    console.log($ps);
+    $('.body p:not(:first-child)').hide();
+    $('.read-on').on('click', function(e) {
+      e.preventDefault();
+      $(this).parent().find('p').fadeIn();
+      $(this).hide();
+    });
   }
 };
 
 blog.articles = [];
 
-$(document).ready(function() {
+$(function() {
   blog.getArticles(blog.rawData);
   blog.dateAndSort();
   blog.publish();
+  blog.hideArticles();
 });
