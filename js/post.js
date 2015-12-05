08c7ddd966fbefd42561e3ea874b63c606da5640
David Swaintek <dswaintek@gmail.com>
2015-12-04 16:37:01 -0800

    Add JSON preview

---
+++ js/post.js
@@ -0,0 +1,26 @@
+//event listener for forms
+$('.newPost > p > *').on('focusout', function () {
+  if (this.name == 'authorUrl') {
+    $('.author').html('<a href="'+this.value+'">'+$('input[name="author"]')[0].value+'</a>');
+  } else {
+    $('.'+this.name).text(this.value);
+  };
+});
+//event listener on checkbox
+$('input[name="publish"]').on('click', function () {
+  if ($(this).is(':checked')) {
+    var postObject = {};
+    var elements = $('.newPost > p > *');
+    for (var i = 0; i < elements.length; i++) {
+      postObject[elements[i].name] = elements[i].value;
+    }
+    var date = new Date();
+    var y = date.getFullYear();
+    var m = date.getMonth()+1;
+    var d = date.getDate();
+    date = y+'-'+m+'-'+d;
+    postObject.publishedOn = date;
+    console.log(postObject);
+    $('#previewTarget').text(JSON.stringify(postObject));
+  }
+})
