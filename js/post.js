813ae85cac9c93429ffe86360a274f2130c126b7
David Swaintek <dswaintek@gmail.com>
2015-12-04 17:17:45 -0800

    Added markdown functionality to post.html

--- js/post.js
+++ js/post.js
@@ -2,6 +2,8 @@
 $('.newPost > p > *').on('focusout', function () {
   if (this.name == 'authorUrl') {
     $('.author').html('<a href="'+this.value+'">'+$('input[name="author"]')[0].value+'</a>');
+  } else if (this.name == 'body'){
+    $('.'+this.name).html(marked(this.value));
   } else {
     $('.'+this.name).text(this.value);
   };
@@ -12,7 +14,7 @@ $('input[name="publish"]').on('click', function () {
     var postObject = {};
     var elements = $('.newPost > p > *');
     for (var i = 0; i < elements.length; i++) {
-      postObject[elements[i].name] = elements[i].value;
+      postObject[elements[i].name] = marked(elements[i].value);
     }
     var date = new Date();
     var y = date.getFullYear();
@@ -23,4 +25,4 @@ $('input[name="publish"]').on('click', function () {
     console.log(postObject);
     $('#previewTarget').text(JSON.stringify(postObject));
   }
-})
+});
