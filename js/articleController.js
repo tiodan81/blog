e3af9d886ae361eb80d83b0cacad45bb71df87cc
Dan Schwartz <dpschwartz@gmail.com>
2015-12-14 14:37:36 -0800

    Initial commit for controller action.

---
+++ js/articleController.js
@@ -0,0 +1,6 @@
+articlesController = {};
+
+articlesController.index = function() {     //initialize index page
+  console.log('router recognized');
+//  Article.loadAll(articleView.index);       //get db if needed, getJSON if no db, build blog.articles(checkfor/get template, renderBlog)
+};
