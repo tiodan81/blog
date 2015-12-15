d1b7809480f2a1bcf33299ece5c3b56bbab59f75
Dan Schwartz <dpschwartz@gmail.com>
2015-12-15 14:41:47 -0800

    Display all GH repos using handlebars template.

--- js/aboutView.js
+++ js/aboutView.js
@@ -1,6 +1,27 @@
 var aboutView = {};
 
 aboutView.index = function() {
-  $('#articles').hide();
-  $('#about').show();
+  var _renderRepos = function() {
+    $('#articles').hide();
+    $('#about').show();
+    repos.all.forEach(function(e) {
+      $('#repos').append(aboutView.toHTML(e));
+    });
+  };
+
+  if (aboutView.template) {
+    _renderRepos();
+  } else {
+    $.get('/templates/repo.html', function(data, msg, xhr) {
+      aboutView.template = Handlebars.compile(data);
+      _renderRepos();
+    });
+  }
+};
+
+aboutView.toHTML = function(repo) {
+  repo.name = repo.name.charAt(0).toUpperCase() + repo.name.slice(1);
+  repo.created_at = repo.created_at.slice(0, 10);
+  repo.updated_at = repo.updated_at.slice(0, 10);
+  return aboutView.template(repo);
 };
--- js/repos.js
+++ js/repos.js
@@ -3,12 +3,12 @@ var repos = {};
 repos.all = [];
 
 repos.requestAll = function(callback) {
+  console.log(token);
   $.ajax({
     type: 'GET',
     url:'https://api.github.com/users/tiodan81/repos?sort=updated',
     headers: {Authorization: 'token ' + token}
   }).done(function(data) {
-    console.log(data);
     repos.all = data;
   }).done(callback);
 };
